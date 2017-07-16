5e4ae09d79ec276693cd60fb25c8175fc8010066
Mohammad Joneidi <joneidi.moh65@gmail.com>
2017-07-16 19:44:39 +0430

    source codes

---
+++ BikeSharing.Zebble/App.UI/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace UI
+{
+    using System;
+    using System.Threading.Tasks;
+    using Zebble;
+    using Zebble.Services;
+
+    public partial class StartUp : Zebble.StartUp
+    {
+        public override async Task Run()
+        {
+            ApplicationName = "AppName2";
+
+            await InstallIfNeeded();
+
+            RegisterDataProvider(typeof(AppData.AdoDotNetDataProviderFactory));
+
+            CssStyles.LoadAll();
+            ImageService.MemoryCacheFolder("Images");
+
+            Device.System.ReceivedMemoryWarning.Handle(() => Alert.Toast("There is a shortage of memory. The application may crash."));
+
+            Services.PushNotificationListener.Setup();
+
+            LoadFirstPage().RunInParallel();
+        }
+
+        public static Task LoadFirstPage() => Nav.Go(new Pages.Page1());
+    }
+}
