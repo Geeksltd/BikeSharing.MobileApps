0508ef7a3e6f82bd017ef11c154e10c8d5030ef6
Mohammad Joneidi <joneidi.moh65@gmail.com>
2017-07-23 10:26:01 +0430

    my rides

---
+++ BikeSharing.Zebble/App.UI/Views/Modules/Generic/MyRidesModule.zbl.cs
@@ -0,0 +1,28 @@
+﻿namespace UI.Modules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Zebble;
+    using Zebble.Framework;
+    using Domain;
+    using Domain.Services;
+
+    partial class MyRidesModule
+    {
+        public List<Ride> Items;
+        public override async Task OnInitializing()
+        {
+            await base.OnInitializing();
+            await InitializeComponents();
+
+            var _ridesService = new RidesService();
+            var ridesResult = await _ridesService.GetUserRides();
+           // Items = ridesResult.ToList();
+        }
+
+
+    }
+}
